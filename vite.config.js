0c9d864a59ab3ae202614910f73346a8ceb06333
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2025-10-26 12:56:59 -0500

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
 	base: '/restaurant_app_1/',
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				signup: resolve(__dirname, 'signup.html'),
+			},
+		},
+	},
 })
